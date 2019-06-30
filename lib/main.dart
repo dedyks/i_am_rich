58b5268dd34780680a2fc7c88831d49fbee26dda
dedy <dedyjoomla@gmail.com>
2019-06-30 19:08:59 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+          home: Scaffold(
+            backgroundColor: Colors.grey,
+            appBar: AppBar(
+
+              title: Text("I Am Rich"),
+              backgroundColor: Colors.blueGrey[900],
+            ),
+            body: Center(
+              child:Image(
+                image: AssetImage('images/diamond.png'),
+                alignment: Alignment.center ,
+                fit: BoxFit.cover,
+              ),
+            )
+          ),
+
+      ));
+
+}
